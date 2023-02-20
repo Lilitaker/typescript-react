20d6e276f92029b2a670dbf446c3a9be63fa6996
Lady Liliana Vasquez Saiz <lady.vasquez@globant.com>
2023-02-20 16:30:07 -0500

    Example of interfaces

---
+++ dist/lib/order.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Order = void 0;
+class Order {
+    calculateTotal() {
+        return 200;
+    }
+}
+exports.Order = Order;
